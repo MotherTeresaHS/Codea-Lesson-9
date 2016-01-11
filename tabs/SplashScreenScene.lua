e4f569add362432b345bb50c3c5e84d60f121460
Mr-Coxall at codea-scm <Mr-Coxall@codea-scm.aws.mapote.com>
2016-01-10 22:35:49 -0500

    initial commit

---
+++ tabs/SplashScreenScene.lua
@@ -0,0 +1,36 @@
+-- SplashScreenScene
+-- Lesson 6
+
+-- Created by: Mr Coxall
+-- Created on: Nov 2014
+-- Created for: ICS2O
+-- This is the first scene
+
+SplashScreenScene = class()
+
+-- global to this file
+local moveForwardButton
+
+function SplashScreenScene:init()
+
+    --sprite("iOS Button Pack:Blue Forward Circle Button")
+    moveForwardButton = Button("iOS Button Pack:Blue Forward Circle Button", vec2(WIDTH/2, HEIGHT/2))
+    
+end
+
+function SplashScreenScene:draw()
+    -- Codea does not automatically call this method
+    
+    background(255, 0, 7, 255)
+    moveForwardButton:draw()
+end
+
+function SplashScreenScene:touched(touch)
+    -- Codea does not automatically call this method
+    
+    moveForwardButton:touched(touch)
+    
+    if(moveForwardButton.selected == true) then
+        Scene.Change("play")
+    end
+end
